e8623b76748d287717c79bfa391a523612fa9dde
abondarenko <aleksei.bondarenko@epassi.com>
2024-06-26 14:56:38 +0300

    Ready to review

--- ImageFeed/ProfileView/ProfileViewController.swift
+++ ImageFeed/ProfileView/ProfileViewController.swift
@@ -10,22 +10,57 @@ import UIKit
 
 final class ProfileViewController: UIViewController {
     
-    var imageView : UIImageView!
-    let profileImage = UIImage(named: "Photo")
-    var userNameLabel : UILabel!
-    var nickNameLabel : UILabel!
-    var descriptionLabel : UILabel!
+    private let profileImage = UIImage(named: "Photo")
     
-    
-    var button : UIButton!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        imageView = UIImageView()
+    private lazy var imageView: UIImageView = {
+        let imageView = UIImageView()
         imageView.translatesAutoresizingMaskIntoConstraints = false
         imageView.image = profileImage
         imageView.tintColor = .gray
+        return imageView
+    }()
+    
+    private lazy var userNameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Екатерина Новикова"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .ypWhite
+        label.font = UIFont.systemFont(ofSize: 23, weight: .bold)
+        return label
+    }()
+    
+    private lazy var nickNameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "@ekaterina_nov"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .ypGrey
+        label.font = UIFont.systemFont(ofSize: 13, weight: .regular)
+        return label
+    }()
+    
+    private lazy var descriptionLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Hello, world!"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .ypWhite
+        label.font = UIFont.systemFont(ofSize: 13, weight: .regular)
+        return label
+    }()
+    
+    private lazy var button: UIButton = {
+        let buttonImage = UIImage(systemName: "ipad.and.arrow.forward")
+        let button = UIButton.systemButton(with: buttonImage!, target: self, action: #selector(buttonTapped))
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.tintColor = .ypRed
+        return button
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+    }
+    
+    private func setupUI() {
         view.addSubview(imageView)
         NSLayoutConstraint.activate([
             imageView.heightAnchor.constraint(equalToConstant: 70),
@@ -34,48 +69,25 @@ final class ProfileViewController: UIViewController {
             imageView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16)
         ])
         
-        userNameLabel = UILabel()
-        userNameLabel.text = "Екатерина Новикова"
-        userNameLabel.translatesAutoresizingMaskIntoConstraints = false
-        userNameLabel.textColor = .ypWhite
-        userNameLabel.font = UIFont.systemFont(ofSize: 23, weight: .bold)
-        
         view.addSubview(userNameLabel)
         NSLayoutConstraint.activate([
             userNameLabel.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 8),
             userNameLabel.leadingAnchor.constraint(equalTo: imageView.leadingAnchor)
         ])
         
-        nickNameLabel = UILabel()
-        nickNameLabel.text = "@ekaterina_nov"
-        nickNameLabel.textColor = .ypGrey
-        nickNameLabel.font = UIFont.systemFont(ofSize: 13, weight: .regular)
-        nickNameLabel.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(nickNameLabel)
         NSLayoutConstraint.activate([
             nickNameLabel.topAnchor.constraint(equalTo: userNameLabel.bottomAnchor, constant: 8),
             nickNameLabel.leadingAnchor.constraint(equalTo: imageView.leadingAnchor)
         ])
         
-        descriptionLabel = UILabel()
-        descriptionLabel.text = "Hello, world!"
-        descriptionLabel.textColor = .ypWhite
-        descriptionLabel.font = UIFont.systemFont(ofSize: 13, weight: .regular)
-        descriptionLabel.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(descriptionLabel)
         NSLayoutConstraint.activate([
             descriptionLabel.topAnchor.constraint(equalTo: nickNameLabel.bottomAnchor, constant: 8),
             descriptionLabel.leadingAnchor.constraint(equalTo: imageView.leadingAnchor)
         ])
         
-        
-        button = UIButton()
-        let buttonImage = UIImage(systemName: "ipad.and.arrow.forward")
-        button = UIButton.systemButton(with: buttonImage!, target: self, action: #selector(buttonTapped))
-        button.tintColor = .ypRed
         view.addSubview(button)
-        button.translatesAutoresizingMaskIntoConstraints = false
-        
         NSLayoutConstraint.activate([
             button.widthAnchor.constraint(equalToConstant: 25),
             button.heightAnchor.constraint(equalToConstant: 25),
@@ -84,7 +96,7 @@ final class ProfileViewController: UIViewController {
         ])
     }
     
-    @objc func buttonTapped() {
+    @objc private func buttonTapped() {
         print("Button was tapped")
     }
 }
--- ImageFeed/SingleImageView/SingleImageViewController.swift
+++ ImageFeed/SingleImageView/SingleImageViewController.swift
@@ -5,41 +5,18 @@
 //  Created by Aleksei Bondarenko on 2.6.2024.
 //
 
-import Foundation
 import UIKit
 
 final class SingleImageViewController: UIViewController {
     var image: UIImage? {
         didSet {
-            guard isViewLoaded, let image else {
-                print("Image not set in didSet")
-                return
-            }
-            imageView.image = image
-            
-            //            imageView.frame.size = image.size
-            //            rescaleAndCenterImageInScrollView(image: image)
-            updateImageViewConstraints()
+            guard isViewLoaded, let image else { return }
+            updateImageView()
         }
     }
     
     @IBOutlet var scrollView: UIScrollView!
     @IBOutlet var imageView: UIImageView!
-    
-    @IBAction func didTapBackButton(_ sender: Any) {
-        dismiss(animated: true, completion: nil)
-    }
-    
-    @IBAction func didTapShareButton(_ sender: UIButton) {
-        guard let image = image else {
-            print("Image is nil")
-            return
-        }
-        let activityViewController = UIActivityViewController(activityItems: [image], applicationActivities: nil)
-        
-        present(activityViewController, animated: true, completion: nil)
-    }
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -48,84 +25,17 @@ final class SingleImageViewController: UIViewController {
         scrollView.minimumZoomScale = 0.1
         scrollView.maximumZoomScale = 1.25
         
-        imageView.contentMode = .scaleAspectFill
-        setupScrollViewConstraints()
-        setupImageViewConstraints()
-        
-        guard let image = image else {
-            print("Image is nil")
-            return
+        if let image = image {
+            updateImageView()
         }
-        imageView.frame.size = UIScreen.main.bounds.size
-        imageView.image = image
-        updateImageViewConstraints()
-        //        rescaleAndCenterImageInScrollView(image: image)
     }
     
-    private func updateImageViewConstraints() {
+    private func updateImageView() {
         guard let image = image else { return }
-        
-        // Рассчитываем новый размер для imageView в зависимости от размеров изображения
-        let imageViewSize = calculateImageViewSize(for: image)
-        
-        // Удаляем старые констрейнты ширины и высоты, если они существуют
-        NSLayoutConstraint.deactivate(imageView.constraints.filter {
-            $0.firstAttribute == .width || $0.firstAttribute == .height
-        })
-        
-        // Устанавливаем новые констрейнты ширины и высоты для imageView
-        let widthConstraint = imageView.widthAnchor.constraint(equalToConstant: imageViewSize.width)
-        let heightConstraint = imageView.heightAnchor.constraint(equalToConstant: imageViewSize.height)
-        
-        widthConstraint.isActive = true
-        heightConstraint.isActive = true
-        
-        // Обновляем contentSize для scrollView
-        scrollView.contentSize = imageViewSize
-        
-        // Центрируем imageView
-        centerImageView()
+        imageView.image = image
+        imageView.frame.size = image.size
+        rescaleAndCenterImageInScrollView(image: image)
     }
-    
-    private func calculateImageViewSize(for image: UIImage) -> CGSize {
-        let widthScale = scrollView.bounds.width / image.size.width
-        let heightScale = scrollView.bounds.height / image.size.height
-        let scale = max(widthScale, heightScale)
-        let width = image.size.width * scale
-        let height = image.size.height * scale
-        return CGSize(width: width, height: height)
-    }
-    
-    private func centerImageView() {
-        let offsetX = max((scrollView.bounds.width - scrollView.contentSize.width) * 0.5, 0.0)
-        let offsetY = max((scrollView.bounds.height - scrollView.contentSize.height) * 0.5, 0.0)
-        imageView.center = CGPoint(x: scrollView.contentSize.width / 2 + offsetX, y: scrollView.contentSize.height / 2 + offsetY)
-    }
-    
-    private func setupScrollViewConstraints() {
-         scrollView.translatesAutoresizingMaskIntoConstraints = false
-         NSLayoutConstraint.activate([
-             // Устанавливаем констрейнты для scrollView относительно его superview
-             scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-             scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-             scrollView.topAnchor.constraint(equalTo: view.topAnchor),
-             scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
-         ])
-     }
-
-    
-    private func setupImageViewConstraints() {
-        imageView.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            // Привязываем imageView к краям scrollView
-            imageView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
-            imageView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
-            imageView.topAnchor.constraint(equalTo: scrollView.topAnchor),
-            imageView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor)
-        ])
-    }
-    
-    
     
     private func rescaleAndCenterImageInScrollView(image: UIImage) {
         let minZoomScale = scrollView.minimumZoomScale
@@ -135,7 +45,7 @@ final class SingleImageViewController: UIViewController {
         let imageSize = image.size
         let hScale = visibleRectSize.width / imageSize.width
         let vScale = visibleRectSize.height / imageSize.height
-        let scale = min(maxZoomScale, max(minZoomScale, min(hScale, vScale)))
+        let scale = min(maxZoomScale, max(minZoomScale, max(hScale, vScale)))
         scrollView.setZoomScale(scale, animated: false)
         scrollView.layoutIfNeeded()
         let newContentSize = scrollView.contentSize
@@ -143,17 +53,29 @@ final class SingleImageViewController: UIViewController {
         let y = (newContentSize.height - visibleRectSize.height) / 2
         scrollView.setContentOffset(CGPoint(x: x, y: y), animated: false)
     }
+    
+    //MARK: - Actions
+    @IBAction func didTapBackButton(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func didTapShareButton(_ sender: UIButton) {
+        guard let image = image else { return }
+        let activityViewController = UIActivityViewController(activityItems: [image], applicationActivities: nil)
+        
+        present(activityViewController, animated: true, completion: nil)
+    }
 }
 
+//MARK: - UIScrollViewDelegate
 extension SingleImageViewController: UIScrollViewDelegate {
     func viewForZooming(in scrollView: UIScrollView) -> UIView? {
         imageView
     }
     
     func scrollViewDidZoom(_ scrollView: UIScrollView) {
-        //                let offsetX = max((scrollView.bounds.width - scrollView.contentSize.width) * 0.5, 0.0)
-        //                let offsetY = max((scrollView.bounds.height - scrollView.contentSize.height) * 0.5, 0.0)
-        //                scrollView.contentInset = UIEdgeInsets(top: offsetY, left: offsetX, bottom: offsetY, right: offsetX)
-        
-        centerImageView()}
+        let offsetX = max((scrollView.bounds.width - scrollView.contentSize.width) * 0.5, 0.0)
+        let offsetY = max((scrollView.bounds.height - scrollView.contentSize.height) * 0.5, 0.0)
+        scrollView.contentInset = UIEdgeInsets(top: offsetY, left: offsetX, bottom: offsetY, right: offsetX)
+    }
 }
